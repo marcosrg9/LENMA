3460415658a0bb20f5b121f16ab2bbefec43dee6
Marcos Rodríguez <marcosylrg@gmail.com>
2021-11-14 13:32:28 +0100

    JSON de actividades añadido. Modificaciones y correcciones menores.

--- ejercicios/receta/carousel.js
+++ ejercicios/receta/carousel.js
@@ -1,23 +1,21 @@
 /**
- * @copyright Copyright (c) 2021 - Marcos Rodríguez Yélamo<marcosylrg@gmail.com>
+ * @author Marcos Rodríguez Yélamo <marcosylrg@gmail.com>
+ * @copyright Copyright (c) 2021 - Marcos Rodríguez Yélamo
  * @license GPLv3
  */
 
+document.body.onload = () => loadCarousel();
+
 /**
- * Carga la referencia al carusel y establece un intérvalo de 5 segundos.
- * Cambia la opacidad del elemento HTML.
- * 
- * Llamar en el evento onload.
+ * Carga la referencia al carousel y establece un intérvalo de 5 segundos entre transición.
  */
 const loadCarousel = () => {
     carousel = document.querySelector('#carousel2');
     
     setInterval(() => {
         const opacity = getComputedStyle(carousel).opacity;
-        if (opacity == 1) {
-            carousel.style.opacity = 0;
-        } else {
-            carousel.style.opacity = 1;
-        }
+        
+        (opacity === '1') ? carousel.style.opacity = 0
+                          : carousel.style.opacity = 1;
     }, 5000);
 }
---
+++ index.js
@@ -0,0 +1,47 @@
+/**
+ * @author Marcos Rodríguez Yélamo <marcosylrg@gmail.com>
+ * @copyright Copyright (c) 2021 - Marcos Rodríguez Yélamo
+ * @license GPLv3
+*/
+
+/**
+ * El administrador de actividades inserta en el contenedor de actividades las tareas realizadas.
+ * Recorre el array activities que contiene todas las tareas realizadas.
+*/
+const activityManager = () => {
+
+    fetch('/activities.json')
+    .then(a => {
+        a.json()
+        .then(body => {
+            
+            const animationFlag = localStorage.getItem('anim');
+            const activityList = document.querySelector('#activityList');
+        
+            // Invierte el array (para visualizar antes las últimas tareas) y lo recorre.
+            body.reverse().forEach( (activity, index) => {
+                
+                // Enlace a la actividad.
+                const link = document.createElement('a');
+                link.href = activity.path;
+        
+                // Entrada de la actividad en la lista.
+                const act = document.createElement('li');
+                act.classList = 'ease-in';
+                if (animationFlag === 'true' || !animationFlag) {
+                    act.style.animationDelay = `${ index*100 }ms`;
+                }
+                // Inserta el nombre de la actividad.
+                act.innerText = activity.name;
+        
+                // Inclusión en la lista de actividades.
+                link.appendChild(act);
+                activityList.appendChild(link);
+        
+            })
+        })
+    })
+}
+
+// Carga el administrador de actividades cuando el body se ha cargado.
+document.body.onload = () => activityManager()
