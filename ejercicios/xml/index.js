f9b90fdeebca185394bda4bbaf04efd07db22a2f
Marcos Rodríguez <marcosylrg@gmail.com>
2022-03-18 13:08:33 +0100

    Tareas DTD añadidas

--- ejercicios/xml/index.js
+++ ejercicios/xml/index.js
@@ -11,58 +11,76 @@ const activityManager = () => {
 
     fetch(url)
     .then(a => {
+
         a.json()
         .then(body => {
             
-            const activityList = document.querySelector('#activityList');
+            const xsdActivity = document.querySelector('#xsdActivityList');
+            const dtdActivity = document.querySelector('#dtdActivityList');
         
             // Invierte el array (para visualizar antes las últimas tareas) y lo recorre.
-            body.reverse().forEach( (activity, index) => {
+            body.forEach( (list, index) => {
                 
-                const { name, path, xml, xsd } = activity;
-                const actName = path.split('/').pop();
-                
-                // Elementos.
-                const cont = document.createElement('div');
-                const title = document.createElement('h4');
-                const linksCont = document.createElement('div');
-                
-                // Enlaces a las actividades.
-                const xmlLink = document.createElement('a');
-                const xsdLink = document.createElement('a');
+                list.reverse().forEach((activity, actIndex) => {
 
-                // Comprueba si hay disponible un fichero .xml
-                if (xml) {
-                    xmlLink.href = `${path}/${actName}.xml`;
-                    xmlLink.innerText = 'XML';
-                    xmlLink.target = '_blank';
-                    linksCont.appendChild(xmlLink);
-                }
+                    const { name, path, xml, task } = activity;
+                    const actName = path.split('/').pop();
 
-                // Comprueba si hay disponible un fichero .xsd
-                if (xsd) {
-                    xsdLink.href = `${path}/${actName}.xsd`;
-                    xsdLink.innerText = 'XSD';
-                    xsdLink.target = '_blank';
-                    linksCont.appendChild(xsdLink);
-                }
-
-                // Define el título de la tarea.
-                title.innerText = name;
-
-                // Define animación.
-                cont.classList = 'ease-in';
-                cont.style.animationDelay = `${ index*100 }ms`;
-                
-                // Añade al contenedor los elementos.
-                cont.appendChild(title);
-                cont.appendChild(linksCont);
-
-                // Añade a la lista de tareas la tarea actual.
-                activityList.appendChild(cont);
+                    console.log(name, index);
+                    
+                    // Elementos.
+                    const cont = document.createElement('div');
+                    const title = document.createElement('h4');
+                    const linksCont = document.createElement('div');
+                    
+                    // Enlaces a las actividades.
+                    const xmlLink  = document.createElement('a');
+                    const taskLink = document.createElement('a');
+    
+                    // Comprueba si hay disponible un fichero .xml
+                    if (xml) {
+                        if (index == 0)      xmlLink.href = `./ejercicios/xsd${path}/${actName}.xml`
+                        else if (index == 1) xmlLink.href = `./ejercicios/dtd${path}/${actName}.xml`;
+                        xmlLink.innerText = 'XML';
+                        xmlLink.target = '_blank';
+                        linksCont.appendChild(xmlLink);
+                    }
+    
+                    // Comprueba si hay disponible un fichero de tarea (xsd o dtd)
+                    if (task) {
+                        if (index == 0) {
+                            taskLink.href = `./ejercicios/xsd/${path}/${actName}.xsd`;
+                            taskLink.innerText = 'XSD';
+                        } else if (index == 1) {
+                            taskLink.href = `./ejercicios/dtd/${path}/${actName}.dtd`;
+                            taskLink.innerText = 'DTD';
+                        }
+                        taskLink.target = '_blank';
+                        linksCont.appendChild(taskLink);
+                    }
+    
+                    // Define el título de la tarea.
+                    title.innerText = name;
+    
+                    // Define animación.
+                    cont.classList = 'ease-in';
+                    cont.style.animationDelay = `${ actIndex*100 }ms`;
+                    
+                    // Añade al contenedor los elementos.
+                    cont.appendChild(title);
+                    cont.appendChild(linksCont);
+    
+                    // Añade a la lista de tareas la tarea actual.
+                    if (index == 0)         xsdActivity.appendChild(cont)
+                    else if (index == 1)    dtdActivity.appendChild(cont);
+                })
         
-            })
+            });
         })
+    })
+    .catch(err => {
+        alert('Error en la petición del archivo de tareas. Consultar consola.');
+        console.error(err);
     })
 }
 
